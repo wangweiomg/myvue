df6f684d0e366a73675bf06056b4668a129d3140
weiw <weiw@opera.com>
2018-12-24 09:54:44 +0800

    menu

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import cardlist from '@/components/card'
+import menu from '@/components/menu'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/card',
       name: 'card',
       component: cardlist
+    },
+    {
+      path: '/menu',
+      name: 'menu',
+      component: menu
     }
   ]
 })
