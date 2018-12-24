9795920bec6f4b68f703a67a120483e3b6a429f3
weiw <weiw@opera.com>
2018-12-24 21:59:59 +0800

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,29 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import cardlist from '@/components/card'
-import Layout from '@/components/Layout'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import cardlist from "@/components/card";
+import Layout from "@/components/Layout";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Layout',
+      path: "/",
+      name: "Layout",
       component: Layout,
       children: [
         {
-          path: '/card',
-          name: 'card',
+          path: "/card",
+          name: "card",
           component: cardlist
-        },
-        {
-          path: '/hello',
-          name: 'HelloWorld',
-          component: HelloWorld
-        },
+        }
       ]
     },
-    
-    
+    {
+      path: "/hello",
+      name: "HelloWorld",
+      component: HelloWorld
+    }
   ]
-})
+});
